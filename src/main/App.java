62767323eb397474d4f737d7d1e7beabb2c8a0d6
poleralph@gmail.com <ralfs.stabins2006@gmail.com>
2025-04-01 11:52:41 +0300

    takes

--- src/main/App.java
+++ src/main/App.java
@@ -11,28 +11,28 @@ import main.Transaction.TransactionType;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        // ArrayList<Transaction> transactions = TransactionManager.getTransactionList();
+        ArrayList<Transaction> transactions = TransactionManager.getTransactionList();
     
-        // for (Transaction transaction : transactions) {
-        //     System.out.println(transaction);
-        // }
-
-        // Transaction transaction = new Transaction(LocalDate.parse("2025-05-02"), TransactionType.INCOMING, "sonders sent monke", "IDK525IDK", 123.25);
-        // System.out.println(transaction);
-
-        // Budget budget = new Budget(BudgetPeriod.MONTHLY, 1000, "Sibder", LocalDate.parse("2025-01-20"));
-        // System.out.println(budget);
-        
-        // Category transport = new Category("Transport", CategoryType.EXPENSE, 250);
-        // System.out.println(transport);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("                      JOSIP FINANCE");
-        System.out.println("------------------------------------------------------");
-        while (true) {
-            System.out.println("Choose your option \n '1' - VIEW YOUR TRANSACTIONS \n");
-            String input = scanner.nextLine();
-            if
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction);
         }
+
+        Transaction transaction = new Transaction(LocalDate.parse("2025-05-02"), TransactionType.INCOMING, "sonders sent monke", "IDK525IDK", 123.25);
+        System.out.println(transaction);
+
+        Budget budget = new Budget(BudgetPeriod.MONTHLY, 1000, "Sibder", LocalDate.parse("2025-01-20"));
+        System.out.println(budget);
+        
+        Category transport = new Category("Transport", CategoryType.EXPENSE, 250);
+        System.out.println(transport);
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("                      JOSIP FINANCE");
+        // System.out.println("------------------------------------------------------");
+        // while (true) {
+        //     System.out.println("Choose your option \n '1' - VIEW YOUR TRANSACTIONS \n");
+        //     String input = scanner.nextLine();
+        //     if
+        // }
 
         
     }
