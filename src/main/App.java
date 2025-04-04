e981092c19be1965a0832b083a04f800fbd967d3
poleralph@gmail.com <ralfs.stabins2006@gmail.com>
2025-04-04 09:32:49 +0300

    it

--- src/main/App.java
+++ src/main/App.java
@@ -10,30 +10,95 @@ import main.Transaction.TransactionType;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in);
+        String clearScreen = "\u001B[2J";
+        System.out.println("                      JOSIP FINANCE");
+        System.out.println("------------------------------------------------------");
+        // Transaction transaction = new Transaction(LocalDate.parse("2025-05-02"), TransactionType.INCOMING, "sonders sent monke", "IDK525IDK", 123.25);
         ArrayList<Transaction> transactions = TransactionManager.getTransactionList();
-    
-        for (Transaction transaction : transactions) {
-            System.out.println(transaction);
-        }
-
-        Transaction transaction = new Transaction(LocalDate.parse("2025-05-02"), TransactionType.INCOMING, "sonders sent monke", "IDK525IDK", 123.25);
-        System.out.println(transaction);
-
         Budget budget = new Budget(BudgetPeriod.MONTHLY, 1000, "Sibder", LocalDate.parse("2025-01-20"));
-        System.out.println(budget);
-        
         Category transport = new Category("Transport", CategoryType.EXPENSE, 250);
-        System.out.println(transport);
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("                      JOSIP FINANCE");
-        // System.out.println("------------------------------------------------------");
-        // while (true) {
-        //     System.out.println("Choose your option \n '1' - VIEW YOUR TRANSACTIONS \n");
-        //     String input = scanner.nextLine();
-        //     if
-        // }
+        while (true) {
+            System.out.println("Choose your option:  \n '1' - VIEW YOUR TRANSACTIONS \n '2' - VIEW YOUR BUDGET \n '3' - VIEW YOUR CATEGORY \n 'X' - EXIT");
+            String input = scanner.nextLine().toUpperCase();
+            switch(input) {
+                case "1": 
+                    System.out.print(clearScreen);
+                    for (Transaction transaction : transactions) {
+                        System.out.println(transaction);
+                    }
 
-        
+                    // System.out.println(transaction);
+                    System.out.println();
+
+                    System.out.println("Choose your option: \n 'Y' - RETURN TO MAIN \n 'X' - EXIT");
+                    String input2 = scanner.nextLine().toUpperCase();
+                    switch(input2) {
+                        case "Y":
+                            System.out.println("Returning to main menu...");
+                            break;
+                        case "X":
+                            System.out.println("Exiting...");
+                            scanner.close();
+                            return;
+                        default:
+                            System.out.println("Invalid input. Please try again.");
+                            break;
+                    } 
+                    break; 
+
+                case "2": 
+                    System.out.print(clearScreen);                 
+                    System.out.println(budget);
+                    System.out.println();
+
+                    System.out.println("Choose your option: \n 'Y' - RETURN TO MAIN \n 'X' - EXIT");
+                    String input3 = scanner.nextLine().toUpperCase();
+                    switch(input3) {
+                        case "Y":
+                            System.out.println("Returning to main menu...");
+                            break;
+                        case "X":
+                            System.out.println("Exiting...");
+                            scanner.close();
+                            return;
+                        default:
+                            System.out.println("Invalid input. Please try again.");
+                            break;
+                    } 
+                    break;
+                case "3": 
+                    System.out.println();
+                    System.out.println(transport);
+                    System.out.println();
+                    System.out.println("Choose your option: \n 'Y' - RETURN TO MAIN \n 'X' - EXIT");
+                    String input4 = scanner.nextLine().toUpperCase();
+                    switch(input4) {
+                        case "Y":
+                            System.out.println("Returning to main menu...");
+                            break;
+                        case "X":
+                            System.out.println("Exiting...");
+                            scanner.close();
+                            return;
+                        default:
+                            System.out.println("Invalid input. Please try again.");
+                            break;
+                    } 
+                    break;
+                case "X":
+                    System.out.println();
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+                
+             
+                default: 
+                    System.out.println();
+                    System.out.println("Invalid input. Please try again.");
+                    System.out.println();
+                    break;
+            }
+        }
     }
 }
